985f7169047911db921de5a7ea2fb99d40f57312
xzomet <enistuzer@gmail.com>
2026-02-01 23:30:40 +0100

    added tests for closed and cancelled orders

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -9,12 +9,6 @@ def test_add_item_creates_single_open_order():
     table_id = 1
     menu_item_id = 1
 
-    r = client.get("/tables/1")
-
-    if r.json()["order_status"] != "closed":
-        order_id = r.json()["order_id"]
-        client.post(f"/orders/{order_id}/close")
-
     # Act 1: add first item (should create order)
     r1 = client.post(
         "/orders/items",
@@ -41,3 +35,53 @@ def test_add_item_creates_single_open_order():
 
     # Assert: same order reused
     assert order_id_1 == order_id_2
+
+
+def test_close_order_marks_closed():
+    table_id = 1
+    menu_item_id = 1
+
+    r1 = client.post(
+        "/orders/items",
+        params={
+            "table_id": table_id,
+            "menu_item_id": menu_item_id,
+            "delta": 1,
+        },
+    )
+    assert r1.status_code == 200
+    order_id_1 = r1.json()["order_id"]
+
+    r2 = client.post(f"/orders/{order_id_1}/close")
+    assert r2.status_code == 200
+    assert r2.json()["status"] == "closed"
+
+
+def test_close_order_marks_zerobill_cancelled():
+    table_id = 1
+    menu_item_id = 1
+
+    r1 = client.post(
+        "/orders/items",
+        params={
+            "table_id": table_id,
+            "menu_item_id": menu_item_id,
+            "delta": 1,
+        },
+    )
+    assert r1.status_code == 200
+    order_id_1 = r1.json()["order_id"]
+
+    r2 = client.post(
+        "/orders/items",
+        params={
+            "table_id": table_id,
+            "menu_item_id": menu_item_id,
+            "delta": -1,
+        },
+    )
+    assert r2.status_code == 200
+
+    r3 = client.post(f"/orders/{order_id_1}/close")
+    assert r3.status_code == 200
+    assert r3.json()["status"] == "cancelled"
